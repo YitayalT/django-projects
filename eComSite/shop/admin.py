987b0bfaf2d200a067304af257ffc49f4210690f
hp <anex518@gmail.com>
2021-08-10 15:11:11 +0300

    first commit

--- eComSite/shop/admin.py
+++ eComSite/shop/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import action
 from .models import Product
 
 # Register your models here.
-admin.site.register(Product)
+class productAdmin(admin.ModelAdmin):
+    
+    def change_category_to_default(self, request, queryset):
+        queryset.update(category = 'default')
+    
+    # change_category_to_default.short_description = "default category"
+    list_display = ('title', 'price', 'discount_price', 'category', 'description')
+    search_fields = ('title',)
+    list_editable = ('price', 'category')
+    actions = ("change_category_to_default")
+
+admin.site.site_header = "E-commerce site"
+admin.site.site_title = "Abc shopping"
+admin.site.index_title  = "manage your shope"
+admin.site.register(Product, productAdmin)
+
+
+
--- eComSite/shop/views.py
+++ eComSite/shop/views.py
@@ -16,7 +16,7 @@ def index(request):
     page = request.GET.get('page')
     product_objects = paginator.get_page(page)
     
-
+    
     return render(request, 'shop/index.html',{'product_objects': product_objects})
 
 def detail(request, id):
